dc8003c733aa249151d8b2a73473a25b7e30bda8
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-28 19:02:00 +0300

    declare heros characteristis

---
+++ src/main/java/models/App.java
@@ -0,0 +1,45 @@
+package models;
+import  static spark.Spark.*;
+import  models.Hero;
+import models.Squad;
+import  spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+//public static void main(String[]args){
+//    staticFileLocation("/public");
+//
+//    get("/",(request, response) -> {
+//        Map<String, Object> MODEL = New HashMap<> ();
+//        int totalHeroes = Hero.getHeroRegistry().size();
+//        int totalSquads = Squad.getAllSquads().size();
+//        int squadlessHeroes = 0;
+//        int squadfullHeroes = 0;
+//        for (Hero hero : Hero.getHeroRegistry()) {
+//            if (hero.getSquadAlliance().equals("")) {
+//                squadlessHeroes += 1;
+//            } else {
+//                squadfullHeroes += 1;
+//            }
+//        }
+//        model.put("totalHeroes", totalHeroes);
+//        model.put("totalSquads", totalSquads);
+//        model.put("squadlessHeroes", squadlessHeroes);
+//        model.put("squadfullHeroes", squadfullHeroes);
+//        model.put("uniqueId", request.session().attribute("uniqueId"));
+//        return new ModelAndView(model, "index.hbs");
+//    },new HandlebarsTemplateEngine());
+//            }
+    }
+
+
+
+
+
+
---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero {
+    private string name;
+    private string age;
+    private string power;
+    private string weakness;
+    private static List<Hero> heroRegistry =new ArrayList<>();
+    private int heroID;
+    private string squadAlliance;
+    private static  boolean duplicate = false;
+
+
+
+
+
+
+}
